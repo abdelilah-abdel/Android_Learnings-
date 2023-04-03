e353717226e9b8864326a401affde3197ef88a6e
abdelilah-abdel <abdelilahezabdel@gmail.com>
2023-04-03 15:28:22 +0000

    volley android added , and get from belatar.name/rest/profile.php?login=test&passwd=test&id=9998

--- app/src/main/java/com/example/tp_android/MainActivity.java
+++ app/src/main/java/com/example/tp_android/MainActivity.java
@@ -3,10 +3,19 @@ package com.example.tp_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String HTTP_URL="https://belatar.name/rest/profile.php?login=test&passwd=test&id=9998";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +25,26 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickHandler(View view) {
         Toast.makeText(this,R.string.ok ,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        JsonObjectRequest request= new JsonObjectRequest(Request.Method.GET, HTTP_URL, null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(MainActivity.class.getSimpleName(),response.toString());
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e(MainActivity.class.getSimpleName(), error.getMessage());
+                    }
+                }
+        );
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
     }
 }
