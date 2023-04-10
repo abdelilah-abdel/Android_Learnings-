89a6744623379a032105e8a8a6048899fd6c38c4
abdelilah-abdel <abdelilahezabdel@gmail.com>
2023-04-10 15:25:16 +0000

    tp3 - kmala

--- app/src/main/java/com/example/tp_android/MainActivity.java
+++ app/src/main/java/com/example/tp_android/MainActivity.java
@@ -5,18 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String HTTP_URL="https://belatar.name/rest/profile.php?login=test&passwd=test&id=9998";
-
+    private static final String HTTP_BASE_URL="https://belatar.name";
+    private static final String HTTP_WS = "/rest/profile.php?login=test&passwd=test&id=9998";
+    private static final String HTTP_IMAGES="/images/";
+    Etudiant etu ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,12 +36,67 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        JsonObjectRequest request= new JsonObjectRequest(Request.Method.GET, HTTP_URL, null,
+        JsonObjectRequest request= new JsonObjectRequest(Request.Method.GET, HTTP_BASE_URL+HTTP_WS, null,
                 new Response.Listener<JSONObject>()
                 {
                     @Override
                     public void onResponse(JSONObject response) {
                         Log.d(MainActivity.class.getSimpleName(),response.toString());
+
+                        try {
+                            if(response.has("error")){
+                                Toast.makeText(MainActivity.this,response.getString("error"),Toast.LENGTH_LONG);
+                            }
+                            else {
+                                etu = new Etudiant(response.getInt("id"), response.getString("nom"),
+                                        response.getString("prenom"),
+                                        response.getString("classe"),
+                                        response.getString("phone"), null);
+
+                                EditText txtNom = findViewById(R.id.txtNom);
+                                EditText textPrenom = findViewById(R.id.txtPrenom);
+                                EditText txtClasse = findViewById(R.id.txtClasse);
+
+                                txtNom.setText(etu.getNom());
+                                textPrenom.setText(etu.getPrenom());
+                                txtClasse.setText(etu.getClasse());
+
+
+                                VolleySingleton.getInstance(getApplicationContext()).getImageLoader().get(
+                                        HTTP_BASE_URL+HTTP_IMAGES+response.getString("photo"),
+                                        new ImageLoader.ImageListener(){
+                                            @Override
+                                            public void onErrorResponse(VolleyError error) {
+
+                                            }
+
+                                            @Override
+                                            public void onResponse(ImageLoader.ImageContainer response, boolean isImmediate) {
+
+                                             }
+
+                                        }
+                                );
+                            new ImageLoader.ImageListener(){
+
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Log.e(MainActivity.class.getSimpleName(),error.getMessage());
+                                }
+
+                                @Override
+                                public void onResponse(ImageLoader.ImageContainer response, boolean isImmediate ) {
+                                    etu.setPhoto(response.getBitmap());
+                                    ImageView img = findViewById(R.id.ImageProfile);
+                                    img.setImageBitmap(etu.getPhoto());
+                                }
+                            };
+
+                            }
+
+                        } catch (JSONException e ){
+                            e.printStackTrace();
+                        }
                     }
                 },
                 new Response.ErrorListener(){
